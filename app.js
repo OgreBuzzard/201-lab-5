33c5b5ac2ee99ee801b33b9d70206e66fc8e5edc
Matt Harding <matt@wherethehellismatt.com>
2017-10-20 15:23:14 -0700

    uploading problem 5

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ function sumArray(testArray){ //eslint-disable-line
 }
 
 // TODO: Here is the test for sumArray(); uncomment it to run it
-testSumArray(testArray);
+// testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -110,10 +110,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(testArray){ //eslint-disable-line
-
+  var theProduct = multiply(testArray[0],testArray[1],testArray[2]);
+  console.log(theProduct);
+  var messageProduct = 'The numbers ' + testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' have a product of ' + theProduct + '.';
+  console.log(messageProduct);
+  return[theProduct, messageProduct];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(2,3,4);
+testMultiplyArray(2,3,4);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
